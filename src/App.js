3fa7d639c85cca71d507fed418407600a3290541
razon1494 <razon1494@gmail.com>
2021-09-24 14:13:35 +0600

    Full Project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+import NavBar from './components/NavBar/NavBar';
+import Slider from './components/slider/Slider';
+import React from 'react';
+import Services from './components/services/Services';
+import Progress from './components/progress/Progress';
+import About from './components/about/About';
+import Form from './components/form/Form';
+import Footer from './components/footer/Footer';
+import Client from './components/Client/Client';
 function App() {
+
   return (
     <div className="App">
-
+      <NavBar />
+      <Slider></Slider>
+      <Services />
+      <Progress />
+      <About />
+      <Form />
+      <Client/>
+      <Footer/>
     </div>
   );
 }
